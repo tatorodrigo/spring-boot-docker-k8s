8a49e9ea35f80a2c496e47e6ebc4c84e2df73063
Rodrigo Botelho <tato.rodrigo@gmail.com>
2018-11-05 17:25:47 -0200

    initial import
    simple spring boot application with two endpoints /hello and
    /hello/delay

---
+++ app/src/main/java/br/com/tattobr/springbootdockerk8s/SpringBootDockerK8sApplication.java
@@ -0,0 +1,12 @@
+package br.com.tattobr.springbootdockerk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerK8sApplication.class, args);
+	}
+}
---
+++ app/src/main/java/br/com/tattobr/springbootdockerk8s/controller/HelloController.java
@@ -0,0 +1,48 @@
+package br.com.tattobr.springbootdockerk8s.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/hello")
+@Log4j2
+public class HelloController {
+    @GetMapping
+    public String sayHello() throws UnknownHostException {
+        log.info(
+                "m=sayHello Now it is {}. Up time: {} ms.",
+                LocalDateTime.now(),
+                ManagementFactory.getRuntimeMXBean().getUptime()
+        );
+        return String.format(
+                "Hello from %s. ",
+                InetAddress.getLocalHost().getHostName()
+        );
+    }
+
+    @GetMapping("/delay")
+    public String sayHelloWithDelay() throws UnknownHostException, InterruptedException {
+        log.info(
+                "m=sayHelloWithDelay Now it is {}. Up time: {} ms. Waiting...",
+                LocalDateTime.now(),
+                ManagementFactory.getRuntimeMXBean().getUptime()
+        );
+        Thread.sleep(5000);
+        log.info(
+                "m=sayHelloWithDelay Now it is {}. Up time: {} ms. Done.",
+                LocalDateTime.now(),
+                ManagementFactory.getRuntimeMXBean().getUptime()
+        );
+        return String.format(
+                "Hello from %s. ",
+                InetAddress.getLocalHost().getHostName()
+        );
+    }
+}
